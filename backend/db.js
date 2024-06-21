046d534c35f0663221ff148f5b7612b5da9b4e88
haroon239 <haroon116butt@gmail.com>
2024-06-21 21:01:39 +0500

    first commit

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 // const url="mongodb+srv://haroon:27february@cluster0.gtkrfen.mongodb.net/autovibes?retryWrites=true&w=majority";
-const url="mongodb://localhost:27017/AutoVibes";
+// const url="mongodb://localhost:27017/AutoVibes";
+const url="mongodb+srv://haroon:AENvLJTC6HKRHMwW@cluster0.gtkrfen.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const mongoose=require("mongoose");
 
 async function connectdb(){
